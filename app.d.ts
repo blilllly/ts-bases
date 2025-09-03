58bfed58b6a580016ac59a3acdcf4f78d66230ce
Billy Alvear <alvearbilly@gmail.com>
2025-09-02 22:44:24 -0500

    first commit

---
+++ app.d.ts
@@ -0,0 +1,6 @@
+declare const msg: string;
+declare const hero: {
+    name: string;
+    age: number;
+};
+//# sourceMappingURL=app.d.ts.map
---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+const msg = "Hola mundo";
+const hero = {
+    name: "ironman",
+    age: 45,
+};
+hero.age = 50;
+console.log(hero.age + 1);
+//# sourceMappingURL=app.js.map
